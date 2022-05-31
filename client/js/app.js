9bce15c6b53e917ccc56ff24d261f013bdbc5188
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-05-31 22:08:55 +0100

    apply changes to front/back to establish working postEntry function

--- client/js/app.js
+++ client/js/app.js
@@ -1,39 +1,55 @@
 // function to update content
-
-window.addEventListener('hashchange', updateContent); //fires when the URL hash changes from one to another
+window.addEventListener("hashchange", updateContent); //fires when the URL hash changes from one to another
+const form = document.querySelector("form");
+form.addEventListener("submit", postEntry);
 
 function updateContent() {
-    const postSection = document.querySelector('#postSection')
-    postSection.innerHTML = ''
-    window.location.hash.substring(1) // returns complete string
+  const postSection = document.querySelector("#postSection");
+  postSection.innerHTML = "";
+  window.location.hash.substring(1); // returns complete string
 }
 
-async function postEntry(e){
-    e.preventDefault();
-    try {
-        const options = {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
-        }
-        const response = await fetch('http://localhost:3000/posts', options);
-        const data = await response.json();
-        window.location.hash = `#${data}` //allows us to change content on page
-    } catch (err) {
-        console.warn(err);
-    }
+async function postEntry(e) {
+  e.preventDefault();
+  // guard clause
+  if (!e.target.title.value) {
+    console.log("no post title");
+    return;
+  }
+  try {
+    const postData = {
+      title: e.target.title.value,
+      name: e.target.name.value,
+      content: e.target.content.value,
+    };
+    console.log(postData)
+    const options = {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: { "Content-Type": "application/json" },
+    };
+
+    fetch("http://localhost:3000/posts", options)
+      .then((r) => r.json())
+    //   .then((data) => {
+    //     data.title = postData.title;
+    //     data.name = postData.name;
+    //     data.content = postData.content;
+    //     const id = data.id;
+    //     // goToPost(id)
+    //   });
+    // window.location.hash = `#${data}`; //allows us to change content on page
+  } catch (err) {
+    console.warn(err);
+  }
 }
 
 async function getPost(id) {
-    try {
-        const response = await fetch(`http://localhost:3000/posts/${id}`);
-        const data = await response.json();
-        return data;
-    } catch (err) {
-        console.warn(err);
-    }
-}
-
-module.exports = {
-    postEntry, getPost
+  try {
+    const response = await fetch(`http://localhost:3000/posts/${id}`);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.warn(err);
+  }
 }
