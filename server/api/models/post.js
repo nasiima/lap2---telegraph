5455f29f2e853020c95040026f3c183f02eee8c1
nasiima <nasiima.mohamed1@gmail.com>
2022-06-01 23:28:12 +0100

    imporving id roue create func

--- server/api/models/post.js
+++ server/api/models/post.js
@@ -27,34 +27,80 @@ class Post {
     });
   }
 
-  static create(title, name, content) {
+  // static create(title, name, content) {
+  //   return new Promise(async (resolve, reject) => {
+  //     try {
+  //       // let post_id = `${timestamp}-${title}`
+  //       // const newPostCreateId = new Post({title,name,content,post_id}) // turns array back into Post
+  //       // const post = dogsData.find(d => d.id === id)
+  //       const db = await init();
+  //       let postData = await db.collection('posts').insertOne({ title, name, content })
+  //       console.log('postData ***************************************************',postData)
+  //       let newPost = new Post(postData.ops[0])
+  //       resolve(newPost);
+  //     } catch (err) {
+  //       reject('Error creating post')
+  //     }
+  //   });
+  // }
+
+
+
+  static create(title,name,content) {
     return new Promise(async (resolve, reject) => {
       try {
-        // let post_id = `${timestamp}-${title}`
-        // const newPostCreateId = new Post({title,name,content,post_id}) // turns array back into Post
-        // const post = dogsData.find(d => d.id === id)
+
         const db = await init();
-        let postData = await db.collection('posts').insertOne({ title, name, content })
-        console.log('postData ***************************************************',postData)
-        let newPost = new Post(postData.ops[0])
+        const sortedPosts = await db
+          .collection("posts")
+          .find()
+          .sort({
+            post_id: -1,
+          })
+          .toArray();
+
+        const newId = sortedPosts.length ? sortedPosts[0].post_id + 1 : 1;
+        let newPost = {
+          post_id: newId,
+          title: title,
+          name: name,
+          content: content,
+        };
+
+        await db.collection("posts").insertOne(newPost);
+
         resolve(newPost);
       } catch (err) {
-        reject('Error creating post')
+        reject(err);
       }
     });
   }
-  static findById(id) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const db = await init()
-        let postData = await db.collection('posts').find({ _id: ObjectId(id)}).toArray() // ObjectId???
-        let post = new Post({...postData[0], id: postData[0]._id}); // postData[0]???
-        resolve(post)
-      } catch (err) {
-        reject('Post not found')
-      }
-    });
-  }
-};
+
+
+
+
+
+
+
+
+
+//   static findById(id) {
+//     return new Promise(async (resolve, reject) => {
+//       try {
+//         const db = await init()
+//         let postData = await db.collection('posts').find({ _id: ObjectId(id)}).toArray() // ObjectId???
+//         let post = new Post({...postData[0], id: postData[0]._id}); // postData[0]???
+//         resolve(post)
+//       } catch (err) {
+//         reject('Post not found')
+//       }
+//     });
+//   }
+// };
+
+
+
+
+
 
 module.exports = Post;
